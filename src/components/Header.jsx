b8bbc9dd232c2ca44595b943d56f34408615afd8
Nuss93 <nasreenrzk@gmail.com>
2023-04-28 12:05:05 +0800

    PRETTIFY

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import AVATAR from '../assets/img/Avatar.png'
 import { Button } from 'reactstrap'
+import AVATAR from '../assets/img/Avatar.png'
+import SEND from '../assets/img/send.png'
+import ADD from '../assets/img/add.png'
 
 export default function Header() {
   return (
@@ -8,12 +10,18 @@ export default function Header() {
       <div className='header'>
         <img src={AVATAR} width={100} style={{position:'absolute', top: '1rem'}} />
           <div style={{marginLeft:'115px', marginRight:'15px', height: '100%'}}>
-            <h2>John Doe</h2>
+            <h4 className='mb-0 fw-bold'>John Doe</h4>
             <small>Last online : 2 days ago</small>
           </div>
           <div>
-            <Button color='light' style={{marginRight:'15px'}}>Send Message</Button>
-            <Button color='light' outline>Add Friend</Button>
+            <Button color='light' style={{marginRight:'15px'}}>
+              <img src={SEND} height={'14px'} />
+              <span style={{color: '#35BAD8', marginLeft:'5px'}}>Send Message</span>
+            </Button>
+            <Button color='light' outline>
+              <img src={ADD} height={'14px'} />
+              <span style={{color: '#fff', marginLeft:'5px'}}>Add Friend</span>
+            </Button>
           </div>
       </div>
     </div>
--- src/components/UserCards.jsx
+++ src/components/UserCards.jsx
@@ -6,9 +6,9 @@ function UserCards(props) {
   return (
     <Card body className='mb-2'>
         <Row className='justify-content-between'>
-            <Col sm={2} className='font-small'>{new Date(props.data.registered.date).toLocaleDateString('en-MY',{day:'2-digit', month:'short', year:'numeric'})}</Col>
+            <Col sm={2} className='font-small' style={{color:'#979797'}}>{new Date(props.data.registered.date).toLocaleDateString('en-MY',{day:'2-digit', month:'short', year:'numeric'})}</Col>
             <Col sm={4} className='font-small'>{props.data.name.first} {props.data.name.last}</Col>
-            <Col sm={1} className='font-small'>{props.data.gender}</Col>
+            <Col sm={1} className='font-small' style={{color:'#979797'}}>{props.data.gender.substring(0, 1).toUpperCase() + props.data.gender.substring(1)}</Col>
             <Col sm={2} className='font-small'>{props.data.location.country}</Col>
             <Col sm={3} className='font-small text-right'>{props.data.email}</Col>
         </Row>
